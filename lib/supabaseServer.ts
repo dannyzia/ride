5e1533ff714dad863514f479b4d37e26bc272704
Ziaur Rahman <digital.papyrus.bd@gmail.com>
2026-06-13 11:46:55 +0600

    2nd Final

--- lib/supabaseServer.ts
+++ lib/supabaseServer.ts
@@ -1,11 +1,15 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
+import ws from "ws";
 
-const supabaseUrl = process.env.SUPABASE_URL ?? '';
-const serviceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY ?? '';
+const supabaseUrl = process.env.SUPABASE_URL ?? "";
+const serviceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY ?? "";
 
 export const supabaseAdmin = createClient(supabaseUrl, serviceRoleKey, {
   auth: {
     autoRefreshToken: false,
     persistSession: false,
+  },
+  realtime: {
+    transport: ws,
   },
 });
